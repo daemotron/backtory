33b70c36e1bed85a28f65010b5af6542d9548fd3
Daemotron <mail@daemotron.net>
2012-01-05 22:44:57 +0100

    Added a simple daemonize method to daemonize the process (only works on unix-like systems)

--- backtory/daemon/base.py
+++ backtory/daemon/base.py
@@ -16,7 +16,22 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 
+import os
+import sys
+
 import backtory.defaults
+
+
+def die(message=''):
+    """
+    Implementation of the poplar die() function shamelessly stolen from PHP
+    This function prints a message (if one has been given) and exits with
+    a status != 0
+    """
+    if message != '':
+        sys.stderr.write("%s\n" % message)
+
+    sys.exit(1)
 
 
 class Daemon(object):
@@ -30,7 +45,40 @@ class Daemon(object):
         pass
 
     def _daemonize(self):
-        pass
+        """
+        This function creates a daemon process (works only on Linux/Unix operating
+        systems). This makes it possible to run this program independently
+        of any terminal.
+        """
+
+        # first fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)    # exit first parent
+        except OSError, e:
+            die("fork #1 failed: ({0:d}) {1:>s}\n".format(e.errno, e.strerror))
+
+        # Set process environment
+        os.chdir("/")
+        os.umask(0)
+        os.setsid()
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)    # Exit second parent.
+        except OSError, e:
+            die("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror))
+
+        # Redirect standard file descriptors.
+        si = open('/dev/null', 'r')
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(si.fileno(), sys.stdout.fileno())
+        os.dup2(si.fileno(), sys.stderr.fileno())
+
+        # Return PID
+        return os.getpid()
 
     def _log_init(self):
         pass
